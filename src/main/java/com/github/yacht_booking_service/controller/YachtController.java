f01087ad2b4af2189d10c1fa8011423a25d6f612
vcshark84 <vcshark84@gmail.com>
2025-06-01 15:51:50 +0300

    Реализована страничка списка яхт

--- src/main/java/com/github/yacht_booking_service/controller/YachtController.java
+++ src/main/java/com/github/yacht_booking_service/controller/YachtController.java
@@ -9,7 +9,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
-
 public class YachtController {
 
     private final YachtService yachtService;
